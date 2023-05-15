2fa41f7e86e1db90270644fec7c9cd7e674df5c8
Peter <schmidt.multimedia@gmail.com>
2023-05-15 09:30:46 +0200

    Basic-Funktionalität / + Array

--- script_01.js
+++ script_01.js
@@ -8,6 +8,17 @@
  */
 
 /*** 02. Theorie: Array */
+let arr;
+// arr = new Array(); // Konstruktor
+arr = []; // Literal
+arr = [2,7,11,4,50];
+
+output(arr);
+output(arr.length); // Anzahl der Elemente
+output(arr[0]); // Index 0 (1. Pos. im Array)
+output(arr[3]); // letze Pos. / konkret
+output(arr[arr.length - 1]);  // letze Pos. / allgemein
+
 
 
 
@@ -21,14 +32,16 @@
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
-
-function getSentence(word1,word2,word3,word4) {
+// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+function getSentence(word1,word2,word3,word4,word5, word6) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
                 word4 + GAP +
+                word5 + GAP +
+                word6 +
                 PUNCT;
 
     return str;
