4e58135031c07d30e54ae1266cccbdfe2334f867
Peter <schmidt.multimedia@gmail.com>
2023-05-15 11:59:14 +0200

    Überlegungen - Transponierung

--- script_01.js
+++ script_01.js
@@ -39,14 +39,50 @@
 // }
 
 /* 02b. For-Schleife für Array-Index (Iteration)*/
-let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
-for (let i = 0; i < arr.length; i++) {
-    output(i);  // innerer Index
-    output(arr[i]); // Array-Index --> Mapping
+// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+// for (let i = 0; i < arr.length; i++) {
+//     output(i);  // innerer Index
+//     output(arr[i]); // Array-Index --> Mapping
+// }
+
+
+/*********   Überlegungen - Transponierung **********/
+
+
+/* 
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 2 + 1
+// output("inhalt von a: " + a);
+
+/* Besser: mit FOR-Schleife */
+// let a = 0; // Anfangswert
+// for (let i = 0; i < 10; i++) { 
+//    // a = a + 1;
+//     a += 1; // Kurzschreibweise
+//     output("inhalt von a: " + a);
+// }
+
+/*
+1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+Solange die Variable existiert, bleibt dieser erhalten
+hier: Verkettung eines Strings // Transponierung
+*/
+let str = ""; // Anfangswert
+for (let i = 0; i < 4; i++) {
+   str += "Test";
+   output("inhalt von str: " + str);
 }
-
-
-
 
 
 
@@ -94,6 +130,27 @@ function getSentenceArr(arr) {
 
     return str;
 }
+
+
+/*** 01b. Funktionalität mit Array 2  */
+// Transponierung:  untereinander ---> nebeneinander
+// Helge Schneider: Anananandereihung ...
+// output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr2(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    // let str =  
+
+    for (let i = 0; i < arr.length; i++) {
+        output(arr[i]); // Array-Index --> Mapping
+    }
+
+
+    //return str;
+}
+
+
+
 
 
 
